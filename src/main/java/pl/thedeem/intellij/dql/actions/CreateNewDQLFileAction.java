7d4c6f215d1ad988204389ce51cc466110269d5a
Dorian Kreft <dorian.krefft@dynatrace.com>
2026-03-05 20:09:40 +0100

    Create new file action now supports all DT extensions (#149)
    * Create new file action now supports all DT extensions
    
    * Create new file action now supports all DT extensions

--- src/main/java/pl/thedeem/intellij/dql/actions/CreateNewDQLFileAction.java
+++ src/main/java/pl/thedeem/intellij/dql/actions/CreateNewDQLFileAction.java
@@ -1,30 +1,121 @@
 package pl.thedeem.intellij.dql.actions;
 
-import com.intellij.ide.actions.CreateFileFromTemplateAction;
-import com.intellij.ide.actions.CreateFileFromTemplateDialog;
+import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.ide.fileTemplates.FileTemplateUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.ui.ComboBox;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.psi.PsiDirectory;
-import org.jetbrains.annotations.NonNls;
+import com.intellij.psi.PsiElement;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBTextField;
+import com.intellij.util.ui.FormBuilder;
+import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import pl.thedeem.intellij.dql.DQLBundle;
-import pl.thedeem.intellij.dql.DQLIcon;
+import pl.thedeem.intellij.dql.services.notifications.DQLNotificationsService;
 
-public class CreateNewDQLFileAction extends CreateFileFromTemplateAction {
-    private final static String DQL_FILE_TEMPLATE = "DQL File.dql";
+import javax.swing.*;
+import java.awt.*;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.util.Set;
 
+public class CreateNewDQLFileAction extends AnAction {
     @Override
-    protected void buildDialog(@NotNull Project project, @NotNull PsiDirectory psiDirectory, CreateFileFromTemplateDialog.@NotNull Builder builder) {
-        builder.setTitle(DQLBundle.message("action.DQL.NewDQLFile.title"))
-                .addKind(
-                        DQLBundle.message("action.DQL.NewDQLFile.kind"),
-                        DQLIcon.DYNATRACE_LOGO,
-                        DQL_FILE_TEMPLATE
-                );
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        PsiElement entryPoint = e.getData(CommonDataKeys.PSI_ELEMENT);
+
+        if (project == null || !(entryPoint instanceof PsiDirectory directory)) return;
+
+        CreateDQLFileDialog dialog = new CreateDQLFileDialog(project, directory);
+        if (dialog.showAndGet()) {
+            String fullName = dialog.getFileName();
+            createFile(project, directory, fullName);
+        }
     }
 
-    @Override
-    protected @NlsContexts.Command String getActionName(PsiDirectory psiDirectory, @NonNls @NotNull String fileName, @NonNls String templateName) {
-        return DQLBundle.message("action.DQL.NewDQLFile.description", fileName);
+    private void createFile(Project project, PsiDirectory directory, String fileName) {
+        FileTemplate template = FileTemplateManager.getInstance(project).getInternalTemplate("DQL File");
+        try {
+            FileTemplateUtil.createFromTemplate(template, fileName, null, directory);
+        } catch (Exception e) {
+            DQLNotificationsService.getInstance(project).showErrorNotification(
+                    DQLBundle.message("action.DQL.NewDQLFile.errors.couldNotCreate.title"),
+                    DQLBundle.message("action.DQL.NewDQLFile.errors.couldNotCreate.message", fileName, e.getMessage()),
+                    project,
+                    Set.of()
+            );
+        }
+    }
+
+    public static class CreateDQLFileDialog extends DialogWrapper {
+        private final JBTextField fileNameField = new JBTextField();
+        private final ComboBox<String> extensionCombo = new ComboBox<>(new String[]{".dql", ".dpl", ".dqlexpr", ".dqlpart"});
+        private final PsiDirectory directory;
+
+        public CreateDQLFileDialog(@Nullable Project project, @NotNull PsiDirectory directory) {
+            super(project);
+            this.directory = directory;
+
+            fileNameField.setPreferredSize(new Dimension(JBUI.scale(250), fileNameField.getPreferredSize().height));
+            setTitle(DQLBundle.message("action.DQL.NewDQLFile.title"));
+            init();
+        }
+
+        @Nullable
+        @Override
+        protected JComponent createCenterPanel() {
+            return FormBuilder.createFormBuilder()
+                    .addLabeledComponent(
+                            new JBLabel(DQLBundle.message("action.DQL.NewDQLFile.form.name")),
+                            new JBLabel(DQLBundle.message("action.DQL.NewDQLFile.form.extension"))
+                    )
+                    .addLabeledComponent(fileNameField, extensionCombo)
+                    .getPanel();
+        }
+
+        @Nullable
+        @Override
+        protected ValidationInfo doValidate() {
+            String name = fileNameField.getText().trim();
+            if (name.isEmpty()) {
+                return new ValidationInfo(DQLBundle.message("action.DQL.NewDQLFile.form.validation.nameEmpty"), fileNameField);
+            }
+            try {
+                Paths.get(name);
+            } catch (InvalidPathException | UnsupportedOperationException ex) {
+                return new ValidationInfo(
+                        DQLBundle.message("action.DQL.NewDQLFile.form.validation.invalidCharacters"),
+                        fileNameField
+                );
+            }
+            String fullName = getFileName();
+            if (directory.findFile(fullName) != null) {
+                return new ValidationInfo(DQLBundle.message("action.DQL.NewDQLFile.form.validation.alreadyExists"), fileNameField);
+            }
+            return null;
+        }
+
+        @Override
+        public JComponent getPreferredFocusedComponent() {
+            return fileNameField;
+        }
+
+        public String getFileName() {
+            String name = fileNameField.getText().trim();
+            String ext = String.valueOf(extensionCombo.getSelectedItem());
+            if (name.endsWith(ext)) {
+                return name;
+            }
+            return name + ext;
+        }
     }
 }
